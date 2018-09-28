3d8923071ac00a5eaa4e313929c8dfb61164fa1c
ShevchukOleg <w.shevchuk.o@gmail.com>
2018-09-28 11:03:31 +0300

    remove mistakes

--- app/js/app.js
+++ app/js/app.js
@@ -28,36 +28,29 @@
 // Строки Задачи
 let string = 'some test string';
 // первая и последняя буквы
-let firstLetter = string.charAt(0);
-let lasrLetter = string.charAt(string.length - 1);
-console.log('First letter is \"' + firstLetter + "\" and the last letter is \"" + lasrLetter +'\"');
-console.log(firstLetter.toUpperCase() + string.slice(1, string.length - 1) + lasrLetter.toUpperCase());
+let firstLetter = string[0];
+let lastLetter = string[string.length - 1];
+console.log('First letter is \"' + firstLetter + "\" and the last letter is \"" + lastLetter +'\"');
+console.log(firstLetter.toUpperCase() + string.slice(1, - 1) + lastLetter.toUpperCase());
 console.log(string.indexOf('string'));
 
 // Конкретно для этой строки подходит метод:
 console.log(string.lastIndexOf(' '));
-// или
-const gap = ' ';
-let position = 0;
-while (true) {
-  let foundPosition = string.indexOf(gap, position);
-  if (foundPosition == -1 ) {break};
-  console.log('Позиция пробела:')
-  console.log(foundPosition);
-  position = foundPosition + 1;
-};
-// Простой вариант
-console.log(string.slice(5,9));
+// Правильный вариант для нахождения второго пробела:
+console.log(string.indexOf(' ', string.indexOf(' ') + 1));
+// Строку с 5 символа
+console.log(string.slice(4,8));
 // А вдруг первый знак будет пробелом или числом, а нам нужны буквы. 
-let firstPos = 5;
+let firstPos = 4;
 let lastPos = 9;
-if (isNaN(string.charAt(5)) == true) {
-console.log(string.slice(firstPos,lastPos));}
-else {console.log(string.slice(firstPos+1,lastPos+1))
+if (string[firstPos] != ' ') {
+  console.log(string.slice(firstPos,lastPos));
+}
+  else {console.log(string.slice(++firstPos,++lastPos));
 };
 // Строку с 5 по 9 символы так понимаю включительно
-console.log(string.slice(5,10));
-let string_2 = string.slice(0, string.length - 6)
+console.log(string.slice(4,10));
+let string_2 = string.slice(0, -6)
 console.log(string_2)
 
 let a = 20;
@@ -74,7 +67,7 @@ console.log('Max value is \"'+ Math.max(15, 11, 16, 12, 51, 12, 13, 51) + '\"' +
 let randomNumber = Math.random();
 console.log(Math.round(randomNumber*100)/100);
 
-let x = +prompt("Please insert the x value" , 1);
+let x = ;
 console.log(Math.random() * x);
 
 let sum = Math.round((0.7+0.6)*10)/10;
