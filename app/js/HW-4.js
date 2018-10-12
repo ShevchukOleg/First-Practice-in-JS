2b60357b5495696f747c64c4fdfd1faab11ae67f
ShevchukOleg <w.shevchuk.o@gmail.com>
2018-10-12 15:11:27 +0300

    Amendment HW-4

--- app/js/HW-4.js
+++ app/js/HW-4.js
@@ -1,69 +1,63 @@
 // Задача 1
-function multiply(){
+function multiply() {
   let result = 1,
-      maxNumber = arguments.length;
-      for (let i = 0; i < maxNumber; i++){
-        result *= arguments[i]; 
-      }
-    return result;
+  maxNumber = arguments.length;
+  if (arguments.length === 0) {
+    result = 0;
+  } else
+  for (let i = 0; i < maxNumber; i++){
+    result *= arguments[i]; 
+  }
+  return result;
 };
 
-console.log(multiply(20, 30, 3));
-// Задача 2
-let str_1 = "Hurricane Michael made landfall on Wednesday afternoon as a category four storm";
-function reverse(){
+// Задача 3
+function reverse(str_1) {
   let resStr_1 = "";
   for (let i = str_1.length-1; i+1; i--){
     resStr_1 += str_1[i];
   }
   return resStr_1;
 }
-console.log(reverse());
-// Задача 4
-let str_2 = "hello";
-function tr(){
-let resStr_2 = "";
-for (let i = 0; i !== str_2.length; i++){
-  resStr_2 += '' + str_2.charCodeAt(i) + " ";
-}
-return resStr_2;
-}
-console.log(tr());
 
-// Масиви
+// Задача 4
+function toUnicode(str_2) {
+  let resStr_2 = "";
+  for (let i = 0; i !== str_2.length; i++){
+    resStr_2 += str_2.charCodeAt(i) + " ";
+  }
+  return resStr_2;
+}
+
+// Масивы
+
 // Задача 1
-let arr = [1, 2, 3];
-function doubleArr(){
-  let doubleArrey = arr.concat(arr);
-return doubleArrey;
+function doubleArr(arr) { 
+  return arr.concat(arr);
 }
-console.log(doubleArr());
+
 // Задача 2
-function lustChar(){
-  let char = arr[arr.length - 1];
-  return char;
+function lustChar(arr) {
+  return arr[arr.length - 1];
 }
-console.log(lustChar());
+
 // Задача 3
-let n = prompt("Введите любое число", 1);
-function count(){
+function countTo(n) {
   let resArr = [];
   let i = 1;
-  while (i <= n){
+  while (i <= n) {
     resArr.push(i);
     i++
   }
   return resArr;
 }
-console.log(count());
 
 // Задача 4
-function kabum(){
+function delFirstSymbol() {
   let resArr_1 = [];
   for (let i = 0; i != arguments.length; i++){
-    arguments[i].shift();
+   arguments[i].shift();
     resArr_1.push(arguments[i]);
   }
   return resArr_1;
 }
-console.log(kabum([1, 2, 3, 4], ["a", "b", "c", "d", "e"], ["A", "B", "C"]));
